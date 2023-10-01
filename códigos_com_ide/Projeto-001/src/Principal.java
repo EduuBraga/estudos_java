47b9bc97f148de08c0071424767359a2b6745f46
Eduardo Braga <eduardo.braga467@gmail.com>
2023-09-30 22:54:02 -0300

    Estudando métodos

--- códigos_com_ide/Projeto-001/src/Principal.java
+++ códigos_com_ide/Projeto-001/src/Principal.java
@@ -5,22 +5,28 @@ public class Principal {
         edu.cpf = "009873900";
         edu.anoNascimento = 2003;
 
-        Moto minhaMoto = new Moto();
-        minhaMoto.anoDeFabricacao = 2014;
-        minhaMoto.modelo = "fan 125";
-        minhaMoto.cor = "vermelha";
-        minhaMoto.fabricante = "honda";
+        Pessoa chalme = new Pessoa();
+        chalme.nome = "Chalme";
+        chalme.cpf = "008762800";
+        chalme.anoNascimento = 39;
 
-        minhaMoto.proprietario = edu;
-//        minhaMoto.proprietario.nome = "Eduardo";
-//        minhaMoto.proprietario.cpf = "009873900";
-//        minhaMoto.proprietario.anoNascimento = 2003;
+        Moto motoEdu = new Moto();
+        motoEdu.anoDeFabricacao = 2014;
+        motoEdu.modelo = "fan 125";
+        motoEdu.cor = "vermelha";
+        motoEdu.fabricante = "honda";
+        motoEdu.precoCompra = 10500;
+        motoEdu.proprietario = edu;
 
-        System.out.printf("Proprietária da moto %s %s %s é: %s",
-            minhaMoto.fabricante,
-            minhaMoto.modelo,
-            minhaMoto.cor,
-            minhaMoto.proprietario.nome
-        );
+        Moto motoChalme = new Moto();
+        motoChalme.anoDeFabricacao = 2016;
+        motoChalme.modelo = "bros 160";
+        motoChalme.cor = "preta";
+        motoChalme.fabricante = "honda";
+        motoChalme.precoCompra = 14000;
+        motoChalme.proprietario = edu;
+
+        motoEdu.imprimirResumoDepreciacao();
+        motoChalme.imprimirResumoDepreciacao();
     }
 }
