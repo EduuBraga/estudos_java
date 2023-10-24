13bdf6f17932d83e6a2fe4699d34549cecd11dfa
Eduardo Braga <eduardo.braga467@gmail.com>
2023-10-23 21:41:00 -0300

    Arrays (Copiando e expandindo)

--- códigos_com_ide/arrays/src/Turma.java
+++ códigos_com_ide/arrays/src/Turma.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
 public class Turma {
     String identificacao;
     String professora;
-    Aluno[] alunos;
+    Aluno[] alunos = new Aluno[0];
+
+    void adicionarAluno(Aluno aluno) {
+        this.alunos = Arrays.copyOf(alunos, alunos.length + 1);
+
+        int ultimoElementoDoArray = this.alunos.length - 1;
+        this.alunos[ultimoElementoDoArray] = aluno;
+    }
+
+    void imprimirListaAlunos() {
+        for (int i = 0; i < alunos.length; i++) {
+            Aluno aluno = alunos[i];
+
+            if(aluno != null) {
+                System.out.printf("%d - %s, %d anos%n", i, aluno.nome, aluno.idade);
+            } else {
+                System.out.printf("%d - Vago%n", i);
+            }
+        }
+    }
 }
