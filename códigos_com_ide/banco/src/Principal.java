68672aaeaf9fc6e4f3d2a7a125247f5ec0a1ef56
Eduardo Braga <eduardo.braga467@gmail.com>
2024-01-07 17:09:44 -0300

    Resolvendo problema de transferencia com polimorfismo

--- códigos_com_ide/banco/src/Principal.java
+++ códigos_com_ide/banco/src/Principal.java
@@ -1,10 +1,23 @@
-import com.github.EduuBraga.banco.Conta;
-import com.github.EduuBraga.banco.ContaEspecial;
-import com.github.EduuBraga.banco.ContaInvestimento;
-import com.github.EduuBraga.banco.Titular;
+import com.github.EduuBraga.banco.*;
 
 public class Principal {
     public static void main(String[] args) {
+        CaixaEletronico caixaEletronico = new CaixaEletronico();
 
+        ContaEspecial contaEspecial1 = new ContaEspecial(new Titular("Eduardo Braga", "07608608456"),
+                7777,
+                12345,
+                90);
+
+        ContaEspecial contaEspecial2 = new ContaEspecial(new Titular("Yasmim Ponciano", "98727866644"),
+                6666,
+                54321,
+                90);
+
+        contaEspecial1.depositar(300);
+
+        caixaEletronico.transferir(contaEspecial1, contaEspecial2, 50);
+        contaEspecial1.imprimirInformacoes();
+        contaEspecial2.imprimirInformacoes();
     }
 }
--- códigos_com_ide/banco/src/com/github/EduuBraga/banco/CaixaEletronico.java
+++ códigos_com_ide/banco/src/com/github/EduuBraga/banco/CaixaEletronico.java
@@ -1,15 +1,17 @@
 package com.github.EduuBraga.banco;
 
 public class CaixaEletronico {
-    public void transferir(ContaEspecial contaOrigem, ContaEspecial contaDestino, double valorTransferencia){
+    public void transferir(Conta contaOrigem, Conta contaDestino, double valorTransferencia){
         final double tarifaDeTransferencia = (valorTransferencia * 2) / 100; // 2%
 
-        System.out.printf("Transferencia de %f.2 de d%/%d para %d/%d%n",
+        System.out.printf("Transferencia de %.2f de %d/%d (%s) para %d/%d (%s)%n",
                 valorTransferencia,
                 contaOrigem.getNumero(),
                 contaOrigem.getAgencia(),
+                contaOrigem.getTitular().getNome(),
                 contaDestino.getNumero(),
-                contaDestino.getAgencia());
+                contaDestino.getAgencia(),
+                contaDestino.getTitular().getNome());
 
         contaOrigem.sacar(valorTransferencia + tarifaDeTransferencia);
         contaDestino.depositar(valorTransferencia);
