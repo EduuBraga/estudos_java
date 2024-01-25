9ab4ffd24e6727c9c714164993e0ff3dc19d4b96
Eduardo Braga <eduardo.braga467@gmail.com>
2024-01-24 22:15:19 -0300

    Feat: Atribuindo a classe ServiceAccountPay uma injeção de dependencia a partir da interface PaymentModel

--- códigos_com_ide/servico_pagamento_de_contas/src/www/github/eduubraga/pagamentodecontas/Main.java
+++ códigos_com_ide/servico_pagamento_de_contas/src/www/github/eduubraga/pagamentodecontas/Main.java
@@ -1,15 +1,22 @@
 package www.github.eduubraga.pagamentodecontas;
 
 import www.github.eduubraga.pagamentodecontas.model.Holerite;
-
 import www.github.eduubraga.pagamentodecontas.model.ServiceOrder;
+
 import www.github.eduubraga.pagamentodecontas.payments.PayableDocument;
 import www.github.eduubraga.pagamentodecontas.payments.Recipient;
+
+import www.github.eduubraga.pagamentodecontas.payments.paymentmodel.PaymentModel;
+import www.github.eduubraga.pagamentodecontas.payments.paymentmodel.Pix;
+import www.github.eduubraga.pagamentodecontas.payments.paymentmodel.Transfer;
+
 import www.github.eduubraga.pagamentodecontas.service.ServiceAccountPay;
 
 public class Main {
     public static void main(String[] args) {
-        ServiceAccountPay serviceAccountPay = new ServiceAccountPay();
+        PaymentModel pix = new Pix();
+        PaymentModel transfer = new Transfer();
+        ServiceAccountPay serviceAccountPay = new ServiceAccountPay(transfer);
 
         Recipient employee = new Recipient("Eduardo", "06503790322", "4567854");
         PayableDocument holerite = new Holerite(employee, 9, 180);
--- códigos_com_ide/servico_pagamento_de_contas/src/www/github/eduubraga/pagamentodecontas/service/ServiceAccountPay.java
+++ códigos_com_ide/servico_pagamento_de_contas/src/www/github/eduubraga/pagamentodecontas/service/ServiceAccountPay.java
@@ -1,17 +1,16 @@
 package www.github.eduubraga.pagamentodecontas.service;
 
 import www.github.eduubraga.pagamentodecontas.payments.PayableDocument;
-import www.github.eduubraga.pagamentodecontas.payments.Recipient;
+import www.github.eduubraga.pagamentodecontas.payments.paymentmodel.PaymentModel;
 
 public class ServiceAccountPay {
+    private final PaymentModel paymentModel;
+
+    public ServiceAccountPay(PaymentModel paymentModel) {
+        this.paymentModel = paymentModel;
+    }
+
     public void pay(PayableDocument document) {
-        Recipient recipient = document.getRecipient();
-
-        if (recipient.notHaveKeyPix()) {
-            throw new RuntimeException("Benéficiario não possui chave pix");
-        }
-
-        System.out.printf("DEBUG: Pix realizado para %s no valor de %.2f com a chave pix %s%n",
-                recipient.getName(), document.getTotalValue(), recipient.getKeyPix());
+        paymentModel.pay(document);
     }
 }
