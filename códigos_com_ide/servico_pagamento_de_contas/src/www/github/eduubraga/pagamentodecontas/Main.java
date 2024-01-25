48804337dc166f99585882e1b36d4a7abc048f29
Eduardo Braga <eduardo.braga467@gmail.com>
2024-01-24 21:24:34 -0300

    Utilizando interface

--- códigos_com_ide/servico_pagamento_de_contas/src/www/github/eduubraga/pagamentodecontas/Main.java
+++ códigos_com_ide/servico_pagamento_de_contas/src/www/github/eduubraga/pagamentodecontas/Main.java
@@ -1,6 +1,8 @@
 package www.github.eduubraga.pagamentodecontas;
 
 import www.github.eduubraga.pagamentodecontas.model.Holerite;
+
+import www.github.eduubraga.pagamentodecontas.model.ServiceOrder;
 import www.github.eduubraga.pagamentodecontas.payments.PayableDocument;
 import www.github.eduubraga.pagamentodecontas.payments.Recipient;
 import www.github.eduubraga.pagamentodecontas.service.ServiceAccountPay;
@@ -10,8 +12,12 @@ public class Main {
         ServiceAccountPay serviceAccountPay = new ServiceAccountPay();
 
         Recipient employee = new Recipient("Eduardo", "06503790322", "4567854");
-        Holerite holerite = new Holerite(employee, 9, 180);
+        PayableDocument holerite = new Holerite(employee, 9, 180);
+
+        Recipient supplier = new Recipient("HP Streetwear", "43275021000144", "4567890");
+        PayableDocument serviceOrder = new ServiceOrder(supplier, 10_000);
 
         serviceAccountPay.pay(holerite);
+        serviceAccountPay.pay(serviceOrder);
     }
 }
