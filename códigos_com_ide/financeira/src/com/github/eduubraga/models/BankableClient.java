9b6472e5b47751e6c5e5d2301f1c35df033f7f72
Eduardo Braga <eduardo.braga467@gmail.com>
2024-01-30 22:14:39 -0300

    refactor: Deixando método mais legível (métodos privados dentro de interfaces)

--- códigos_com_ide/financeira/src/com/github/eduubraga/models/BankableClient.java
+++ códigos_com_ide/financeira/src/com/github/eduubraga/models/BankableClient.java
@@ -7,12 +7,20 @@ public interface BankableClient {
     String getName();
 
     default double calculateInterest(double requestedAmount) {
-        if (requestedAmount <= 100_000) {
+        if (isSmallAmountFinancing(requestedAmount)) {
             return 1.0;
-        } else if (requestedAmount <= 1_000_000) {
+        } else if (isMediumAmountFinancing(requestedAmount)) {
             return 1.5;
         }
 
-        return 2.0;
+        return 2.0; // big amount financing
+    }
+
+    private static boolean isMediumAmountFinancing(double requestedAmount) {
+        return requestedAmount <= 1_000_000;
+    }
+
+    private static boolean isSmallAmountFinancing(double requestedAmount) {
+        return requestedAmount <= 100_000;
     }
 }
