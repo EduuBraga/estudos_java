6be23da7fd8188a4fbb389f15a30cdee398ddcd6
Eduardo Braga <eduardo.braga467@gmail.com>
2024-02-06 20:57:00 -0300

    Chore: Utilizando a implementação da interface seguro na classe locação e testando

--- códigos_com_ide/locadora-notebooks/src/com/github/eduubraga/rentalcompany/Location.java
+++ códigos_com_ide/locadora-notebooks/src/com/github/eduubraga/rentalcompany/Location.java
@@ -1,12 +1,23 @@
 package com.github.eduubraga.rentalcompany;
 
+import java.util.Objects;
+
 public class Location {
     private Notebook notebook;
     private Pricing pricing;
+    private Safe safe;
 
     public Location(Notebook notebook, Pricing pricing) {
+        Objects.requireNonNull(notebook);
+        Objects.requireNonNull(pricing);
+
         this.notebook = notebook;
         this.pricing = pricing;
+    }
+
+    public Location(Notebook notebook, Pricing pricing, Safe safe) {
+        this(notebook, pricing);
+        this.safe = safe;
     }
 
     public Notebook getNotebook() {
@@ -17,8 +28,20 @@ public class Location {
         return pricing;
     }
 
+    public Safe getSafe() {
+        return safe;
+    }
+
+    private boolean haveInsurance() {
+        return safe != null;
+    }
+
     public double calcAmountDue(int hoursUsed) {
         double result = getPricing().calculate(getNotebook(), hoursUsed);
+
+        if (haveInsurance()) {
+            result += getSafe().calcPremium(hoursUsed, result);
+        }
 
         return result;
     }
