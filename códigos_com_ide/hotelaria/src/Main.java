5cb060de65484ed1ff45a4fc0281ce73041dcc96
Eduardo Braga <eduardo.braga467@gmail.com>
2024-02-22 22:32:54 -0300

    Feat: iterando ArrayList com for tradicional

--- códigos_com_ide/hotelaria/src/Main.java
+++ códigos_com_ide/hotelaria/src/Main.java
@@ -1,18 +1,29 @@
 import com.github.eduubraga.agencia.Hotel;
 import com.github.eduubraga.agencia.HotelRegistration;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         HotelRegistration hotelRegistration = new HotelRegistration();
 
-        hotelRegistration.addHotel("Hotel miami", "miami", 4_000);
-        hotelRegistration.addHotel("palace", "pentecoste/ce", 500);
-        hotelRegistration.addHotel("Casa lunar", "fortaleza/ce", 900);
+        hotelRegistration.addHotel("Hotel miami" , "miami" , 4_000);
+        hotelRegistration.addHotel("palace" , "pentecoste/ce" , 500);
+        hotelRegistration.addHotel("Casa lunar" , "fortaleza/ce" , 900);
 
-        System.out.println(hotelRegistration.getAll());
+        ArrayList hotels = hotelRegistration.getAll();
+        printHotels(hotels);
+    }
 
-        Hotel palace = (Hotel) hotelRegistration.getAll().get(1);
+    public static void printHotels(ArrayList hotels) {
+        for (Object hotelCurrent : hotels) {
+            Hotel hotel = (Hotel) hotelCurrent;
+            System.out.printf("%s (%s) -> %.2f%n" , hotel.getName(), hotel.getCity(), hotel.getDailyPrice());
+        }
 
-        System.out.println(palace.getName());
+//        for (int i = 0; i < hotels.size(); i++) {
+//            Hotel hotel = (Hotel) hotels.get(i);
+//            System.out.printf("%s (%s) -> %.2f%n", hotel.getName(), hotel.getCity(), hotel.getDailyPrice());
+//        }
     }
 }
