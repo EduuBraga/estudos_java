69331af070af6cd69b2f89fddebfadfe29eca04b
Eduardo Braga <eduardo.braga467@gmail.com>
2024-03-12 20:09:56 -0300

    Chore: Tornando método de remover todos coringa (utilizando "?" - generics)

--- códigos_com_ide/generics/src/Main3.java
+++ códigos_com_ide/generics/src/Main3.java
@@ -9,15 +9,23 @@ public class Main3 {
         products.toPlace(new Product("Cafe Santa c. Almof. 250g C/10", 78.10));
 
         removeAll(products);
+
+        MyCollection<String> names = new Row<>();
+
+        names.toPlace("Eduardo");
+        names.toPlace("Vitória");
+        names.toPlace("Ramon");
+
+        removeAll(names);
     }
 
-    public static void removeAll(MyCollection<Product> products) {
+    public static void removeAll(MyCollection<?> items) {
         try {
             int counter = 1;
 
             while (true) {
-                Product itemRemoved = products.toRemove();
-                System.out.printf("%d. %s%n", counter, itemRemoved.getName());
+                Object itemRemoved = items.toRemove();
+                System.out.printf("%d. %s%n", counter, itemRemoved);
                 counter++;
             }
         } catch (EmptyStackException e) {
