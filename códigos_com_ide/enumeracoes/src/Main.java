6844a5a1aab89cfdd92b152401a2684de5ede769
Eduardo Braga <eduardo.braga467@gmail.com>
2024-04-06 21:52:49 -0300

    Conferindo métodos do enum

--- códigos_com_ide/enumeracoes/src/Main.java
+++ códigos_com_ide/enumeracoes/src/Main.java
@@ -3,10 +3,27 @@ import com.github.eduubraga.RequestStatus;
 
 public class Main {
     public static void main(String[] args) {
-        Request pedido = new Request("eduu");
+//        Request pedido = new Request("eduu");
+//
+//        pedido.setStatus(RequestStatus.INVOICED);
+//
+//        System.out.println(pedido.getStatus());
 
-        pedido.setStatus(RequestStatus.INVOICED);
+        System.out.println(RequestStatus.INVOICED.ordinal());
+        System.out.println(RequestStatus.INVOICED.name());
 
-        System.out.println(pedido.getStatus());
+        for (RequestStatus status : RequestStatus.values()) {
+            System.out.printf("%d - %s%n", status.ordinal(), status.name());
+        }
+
+        // se recebermos um texto de fora podemos pegar o enum a partir dele.
+        String text = "CANCELED";
+        RequestStatus result = RequestStatus.valueOf(text);
+        System.out.println(result);
+
+        // se quisermos pesquisar por número podemos utilizar o values
+        int number = 1;
+        RequestStatus resultNumber = RequestStatus.values()[number];
+        System.out.println(resultNumber);
     }
 }
