75ad8a071e8ed3e73de0e9dd144f873677954b6d
Eduardo Braga <eduardo.braga467@gmail.com>
2024-05-27 14:48:04 -0300

    Copiando arquivos com OutputStream (IO clássica)

--- códigos_com_ide/api-classica-io/src/Main5.java
+++ códigos_com_ide/api-classica-io/src/Main5.java
@@ -2,13 +2,16 @@ import java.io.*;
 
 public class Main5 {
     public static void main(String[] args) {
-        File file = new File("docs\\hello.txt");
+        File fileOrigin = new File("docs\\hello.txt");
+        File fileDestiny = new File("docs\\hello2.txt");
 
-        try (InputStream inputStream = new FileInputStream(file)) {
-            int byteRead;
+        try (InputStream inputStream = new FileInputStream(fileOrigin);
+            OutputStream outputStream = new FileOutputStream(fileDestiny)) {
+            byte[] bytesRead = new byte[1024];
+            int quantityBytesRead;
 
-            while ((byteRead = inputStream.read()) != -1) {
-                System.out.print((char) byteRead);
+            while ((quantityBytesRead = inputStream.read(bytesRead)) > 0) {
+                outputStream.write(bytesRead, 0, quantityBytesRead);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
